400a0201046890d62d076a52c64cf10d3b936bd8
LayloMirdadoeva <laylomirdadoeva@gmail.com>
2023-03-22 08:45:26 -0700

    Create request, response, validation, service for Sender

---
+++ Delivery.Application/Common/Interfaces/ISenderService.cs
@@ -0,0 +1,15 @@
+﻿using Delivery.Application.Requests.SenderRequest;
+using Delivery.Application.Respons.SenderResponse;
+using Delivery.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery.Application.Common.Interfaces
+{
+    public interface ISenderService : IBaseService<Sender, SenderResponse, CreateSenderRequest>
+    {
+    }
+}
