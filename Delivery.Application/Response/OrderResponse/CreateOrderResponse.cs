76aa47ed834d20dc9cabe30b41ab1653aac1168b
Shabon Sharipov <shabonsharipov@outlook.com>
2023-03-27 18:05:44 +0500

    deleted CreateOrderResponse

--- Delivery.Application/Response/OrderResponse/CreateOrderResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Delivery.Application.Respons.OrderResponse;
-
-public class CreateOrderResponse : OrderResponse
-{
-    public string PhoneNumber { get; set; }
-    public string IsPayment { get; set; }
-    public decimal TotalPrice { get; set; }
-    public ulong SenderId { get; set; }
-}
