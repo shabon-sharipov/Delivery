9cb9622c03552cfd6c40d10ba6557792f982b222
Shabon Sharipov <shabonsharipov@outlook.com>
2023-03-29 00:25:04 +0500

    murge confict

--- Delivery.API/Program.cs
+++ Delivery.API/Program.cs
@@ -2,7 +2,10 @@ using Delivery.Application.Common.Interfaces;
 using Delivery.Application.Common.Interfaces.Repositories;
 using Delivery.Application.Mappers;
 using Delivery.Application.Services;
+using Delivery.Application.Validations.CreateCategoryCustomerRequestValidation;
+using Delivery.Application.Validations.OrderValidations;
 using Delivery.Application.Validations.ProductValidations;
+using Delivery.Application.Validations.SenderValidations;
 using Delivery.Infrastructure.Persistence.Database;
 using Delivery.Infrastructure.Persistence.Repositories;
 using FluentValidation;
@@ -37,6 +40,10 @@ namespace Delivery.API
             builder.Services.AddFluentValidationAutoValidation();
             builder.Services.AddValidatorsFromAssembly(typeof(CreateProductValidation).Assembly);
             builder.Services.AddValidatorsFromAssembly(typeof(UpdateProductValidation).Assembly);
+            builder.Services.AddValidatorsFromAssembly(typeof(CreateMerchantCategoryRequestValidation).Assembly);
+            builder.Services.AddValidatorsFromAssembly(typeof(CreateDriverValidation).Assembly);
+            builder.Services.AddValidatorsFromAssembly(typeof(UdateDriverValidation).Assembly);
+            builder.Services.AddValidatorsFromAssembly(typeof(CreateOrderRequestValidation).Assembly);
 
             //AvtoMapper
             builder.Services.AddAutoMapper(typeof(AutoMapperConfiguration).Assembly);
