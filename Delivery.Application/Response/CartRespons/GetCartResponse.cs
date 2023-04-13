ee4a17017cf786cd5b120cbbbcbd257c73903c12
Shabon Sharipov <shabonsharipov@outlook.com>
2023-04-13 18:31:42 +0500

    Changed List<CartItem> to List<GetCardItemResponse>

--- Delivery.Application/Response/CartRespons/GetCartResponse.cs
+++ Delivery.Application/Response/CartRespons/GetCartResponse.cs
@@ -1,9 +1,10 @@
 ﻿using Delivery.Application.Response;
+using Delivery.Application.Response.CardItemResponse;
 using Delivery.Application.Response.CartRespons;
 
 public class GetCartResponse : CartResponse
 {
-    public virtual List<CartItem> CardItems { get; set; }
+    public virtual List<GetCardItemResponse> CardItems { get; set; }
     public decimal TotalPrice { get; set; }
     public ulong CurrentUserId { get; set; }
 }
